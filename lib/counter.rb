08051fafc7258a394524d25ebc53d8d8a02b2689
Tatsiana Ivonchyk <tatsiana@makers.tech>
2020-09-30 15:02:20 +0100

    Update count in database

--- lib/counter.rb
+++ lib/counter.rb
@@ -2,14 +2,6 @@ require 'pg'
 
 class Counter
 
-  def initialize
-    @count = 0
-  end
-
-  def increment
-    @count += 1
-  end
-
   def count
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'count_manager_test')
@@ -19,6 +11,17 @@ class Counter
 
     result = connection.exec("SELECT * FROM counter WHERE id=1;")
     result[0]['count'].to_i
+  end
+
+  def increment
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'count_manager_test')
+    else
+      connection = PG.connect(dbname: 'count_manager')
+    end
+
+    read_count = count
+    result = connection.exec("UPDATE counter SET count = '#{read_count + 1}' WHERE id=1;")
   end
 
   def self.instance
