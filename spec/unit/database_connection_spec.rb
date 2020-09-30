65310d9520ec47d1510be6cee6603f2597e6fbcf
Tatsiana Ivonchyk <tatsiana@makers.tech>
2020-09-30 15:21:54 +0100

    Add query method to DatabaseConnection

--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -15,4 +15,14 @@ describe DatabaseConnection do
       expect(DatabaseConnection.connection).to eq connection
     end
   end
+
+  describe '.query' do
+    it 'executes a query via PG' do
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+
+      expect(connection).to receive(:exec).with("SELECT * FROM bookmarks;")
+
+      DatabaseConnection.query("SELECT * FROM bookmarks;")
+    end
+  end
 end
