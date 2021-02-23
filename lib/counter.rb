258d9b4a189b85c133e40be490c4f3d4abc58ee0
Christine Chee <christinechee88@hotmail.com>
2021-02-23 11:52:47 +0000

    Update app to display last updated time

--- lib/counter.rb
+++ lib/counter.rb
@@ -6,17 +6,28 @@ class Counter
     result[0]['count'].to_i
   end
 
+  def updatedtime
+    result = DatabaseConnection.query("SELECT * FROM counter WHERE id=1;")
+    result[0]['time']
+  end
+
   def increment
     read_count = count
     result = DatabaseConnection.query("UPDATE counter SET count = '#{read_count + 1}' WHERE id=1;")
+    update_time
   end
 
   def decrement
     read_count = count
     result = DatabaseConnection.query("UPDATE counter SET count = '#{read_count - 1}' WHERE id=1;")
+    update_time
   end
 
   def self.instance
     @counter ||= Counter.new
+  end
+
+  def update_time
+    DatabaseConnection.query("UPDATE counter SET time = LOCALTIME(0) WHERE id=1;")
   end
 end
